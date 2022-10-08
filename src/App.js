f0bc8fe7c635865de0c843cea60e5c596fa9a77a
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-10-08 16:12:26 +0100

    add: initial raw layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div>
+      <header>
+        <h1>StudyDeck</h1>
+        <h2>Retention through repitition</h2>
+      </header>
+      <main>
+        <h3>Your Cards</h3>
+        <div>
+          <h4>Term goes here</h4>
+          <div>
+            <button type="button">show back</button>
+            <button type="button">edit</button>
+            <button type="button">delete</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
