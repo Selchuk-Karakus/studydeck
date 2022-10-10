dea18044dd49f4931b0cc3fbedba9bdf5df4831c
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-10-10 21:46:34 +0100

    test: define mock data and mock a fetch API call to data

---
+++ src/tests/App.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, act } from "@testing-library/react";
+import App from "../App";
+import mockCards from "./mocks/MockResponse";
+import getCards from "../service/cardService";
+
+jest.mock("../service/cardService");
+describe("should render App component", () => {
+  it("Renders cards from the API", async () => {
+    getCards.mockResolvedValue(mockCards);
+
+    let getByText;
+    await act(async () => {
+      ({ getByText } = render(<App />));
+    });
+
+    mockCards.forEach((card) => {
+      expect(getByText(card.term)).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/tests/mocks/MockResponse.js
@@ -0,0 +1,39 @@
+const mockCards = [
+  {
+    term: "Question 1",
+    definition: "Answer 1",
+    id: "45e78cbb-295a-46b1-97e0-dd5bbe18e550",
+  },
+  {
+    term: "Question 2",
+    definition: "Answer 2",
+    id: "b72ecca6-b00c-4353-9e51-e04d5e878569",
+  },
+  {
+    term: "Question 3",
+    definition: "Answer 4",
+    id: "a50cc03d-69a7-4313-9596-261d1b2a39b6",
+  },
+  {
+    term: "test 1",
+    definition: "Testing 1",
+    id: "605500eb-33ff-43f2-9a07-18d586a1854d",
+  },
+  {
+    term: "test 2",
+    definition: "Testing 2",
+    id: "78739875-d9a6-4655-9c40-58780729ea56",
+  },
+  {
+    term: "test 3",
+    definition: "Testing 3",
+    id: "f368baee-f675-4b39-adc5-93083dadf074",
+  },
+  {
+    term: "test 4",
+    definition: "Testing 4",
+    id: "208df4bc-88af-485f-92a7-54fc4237e7f7",
+  },
+];
+
+export default mockCards;
